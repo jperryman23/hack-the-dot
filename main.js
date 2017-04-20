5451822f48da9a7b8c3e72becc87bd951baeec81
Jules Perryman <jules.perryman@gmail.com>
2017-04-20 12:13:27 -0600

    added files

---
+++ main.js
@@ -0,0 +1,6 @@
+$('#button').click(function(){
+    let name = $('#name').val()
+    let type = $('#type').val()
+    let message = $('#message').val()
+    window.location.href = 'certificate.html'
+})
